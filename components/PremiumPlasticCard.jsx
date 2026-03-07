7eb062e42e95463a8cc528db8cc9b900f425a945
Abdul <abdul@example.com>
2026-03-08 00:19:42 +0100

    push

--- components/PremiumPlasticCard.jsx
+++ components/PremiumPlasticCard.jsx
@@ -121,49 +121,52 @@ export default function PremiumPlasticCard({ user, qrCodeData, forwardedRef }) {
                 <div className={`premium-card-inner ${isFlipped ? 'is-flipped' : ''}`}>
                     {/* Front of card */}
                     <div className="premium-card-front">
-                        {/* Background SVG */}
+                        {/* Background SVG - Always visible on front */}
                         <img src="/premium.svg" className="absolute inset-0 w-full h-full object-cover" alt="" />
                         
-                        {/* Header Text (simulated) */}
-                        <div className="absolute top-[18px] left-[150px] transform -translate-x-1/2 text-[15px] font-black text-[#006400] whitespace-nowrap z-10" style={{ backfaceVisibility: 'hidden' }}>
-                            FEDERAL REPUBLIC OF NIGERIA
-                        </div>
-                        <div className="absolute top-[35px] left-[95px] transform -translate-x-1/2 text-[13px] font-bold text-slate-800 uppercase tracking-tighter z-10" style={{ backfaceVisibility: 'hidden' }}>
-                            Digital NIN Slip
-                        </div>
+                        {/* Conditional Wrapper for Data: Hides content manually when flipped to prevent mobile 'ghosting' */}
+                        <div className={`absolute inset-0 transition-opacity duration-300 ${isFlipped ? 'opacity-0' : 'opacity-100'}`}>
+                            {/* Header Text (simulated) */}
+                            <div className="absolute top-[18px] left-[150px] transform -translate-x-1/2 text-[15px] font-black text-[#006400] whitespace-nowrap z-10" style={{ backfaceVisibility: 'hidden' }}>
+                                FEDERAL REPUBLIC OF NIGERIA
+                            </div>
+                            <div className="absolute top-[35px] left-[95px] transform -translate-x-1/2 text-[13px] font-bold text-slate-800 uppercase tracking-tighter z-10" style={{ backfaceVisibility: 'hidden' }}>
+                                Digital NIN Slip
+                            </div>
 
-                        {/* Photo */}
-                        <div className="absolute top-[75px] left-[30px] w-[90px] h-[110px] bg-slate-100 border border-slate-200 overflow-hidden z-10" style={{ backfaceVisibility: 'hidden' }}>
-                            {user?.photo && <img src={user.photo} className="w-full h-full object-cover" alt="User" />}
-                        </div>
+                            {/* Photo */}
+                            <div className="absolute top-[75px] left-[30px] w-[90px] h-[110px] bg-slate-100 border border-slate-200 overflow-hidden z-10" style={{ backfaceVisibility: 'hidden' }}>
+                                {user?.photo && <img src={user.photo} className="w-full h-full object-cover" alt="User" />}
+                            </div>
 
-                        {/* QR Code */}
-                        <div className="absolute top-[20px] right-[20px] w-[90px] h-[100px] bg-white p-1 flex items-center justify-center z-10" style={{ backfaceVisibility: 'hidden' }}>
-                            {qrImage && <img src={qrImage} className="w-full h-full object-contain" alt="QR" />}
-                        </div>
+                            {/* QR Code */}
+                            <div className="absolute top-[20px] right-[20px] w-[90px] h-[100px] bg-white p-1 flex items-center justify-center z-10" style={{ backfaceVisibility: 'hidden' }}>
+                                {qrImage && <img src={qrImage} className="w-full h-full object-contain" alt="QR" />}
+                            </div>
 
-                        {/* Data Fields */}
-                        <div className="premium-label top-[85px] left-[140px]">Surname/Nom</div>
-                        <div className="premium-value top-[100px] left-[140px]">{user?.lastName || "ADEBAYO"}</div>
+                            {/* Data Fields */}
+                            <div className="premium-label top-[85px] left-[140px]">Surname/Nom</div>
+                            <div className="premium-value top-[100px] left-[140px]">{user?.lastName || "ADEBAYO"}</div>
 
-                        <div className="premium-label top-[125px] left-[140px]">Given Names/Prenoms</div>
-                        <div className="premium-value top-[140px] left-[140px]">{user?.firstName || "CHUKWUMA"} {user?.middleName || ""}</div>
+                            <div className="premium-label top-[125px] left-[140px]">Given Names/Prenoms</div>
+                            <div className="premium-value top-[140px] left-[140px]">{user?.firstName || "CHUKWUMA"} {user?.middleName || ""}</div>
 
-                        <div className="premium-label top-[165px] left-[140px]">Date of Birth</div>
-                        <div className="premium-value top-[180px] left-[140px]">{formatDOB(user?.dob) || "15 MAY 1990"}</div>
+                            <div className="premium-label top-[165px] left-[140px]">Date of Birth</div>
+                            <div className="premium-value top-[180px] left-[140px]">{formatDOB(user?.dob) || "15 MAY 1990"}</div>
 
-                        <div className="premium-label top-[165px] left-[260px]">Sex/Sexe</div>
-                        <div className="premium-value top-[180px] left-[260px]">{user?.gender?.charAt(0) || "M"}</div>
+                            <div className="premium-label top-[165px] left-[260px]">Sex/Sexe</div>
+                            <div className="premium-value top-[180px] left-[260px]">{user?.gender?.charAt(0) || "M"}</div>
 
-                        <div className="premium-label top-[165px] right-[40px] text-[16px] font-black text-slate-400 opacity-50">NGA</div>
+                            <div className="premium-label top-[165px] right-[40px] text-[16px] font-black text-slate-400 opacity-50">NGA</div>
 
-                        <div className="premium-label top-[175px] right-[40px] font-bold text-[10px]">Issue Date</div>
-                        <div className="premium-value top-[188px] right-[40px] text-[10px] font-light whitespace-nowrap text-right">{formatIssueDate()}</div>
+                            <div className="premium-label top-[175px] right-[40px] font-bold text-[10px]">Issue Date</div>
+                            <div className="premium-value top-[188px] right-[40px] text-[10px] font-light whitespace-nowrap text-right">{formatIssueDate()}</div>
 
-                        {/* NIN Display */}
-                        <div className="absolute top-[215px] left-0 w-full text-center text-[10px] text-slate-500 font-bold uppercase z-10" style={{ backfaceVisibility: 'hidden' }}>National Identification Number (NIN)</div>
-                        <div className="absolute top-[228px] left-0 w-full text-center text-[36px] font-black tracking-[0.2em] text-black z-10" style={{ backfaceVisibility: 'hidden' }}>
-                            {formatNIN(user?.nin) || "0000 0000 000"}
+                            {/* NIN Display */}
+                            <div className="absolute top-[215px] left-0 w-full text-center text-[10px] text-slate-500 font-bold uppercase z-10" style={{ backfaceVisibility: 'hidden' }}>National Identification Number (NIN)</div>
+                            <div className="absolute top-[228px] left-0 w-full text-center text-[36px] font-black tracking-[0.2em] text-black z-10" style={{ backfaceVisibility: 'hidden' }}>
+                                {formatNIN(user?.nin) || "0000 0000 000"}
+                            </div>
                         </div>
                     </div>
 
