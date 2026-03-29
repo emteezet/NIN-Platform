5f371222213169a4ac78b410aaf712b789fab86a
Abdul <abdul@example.com>
2026-03-29 12:41:31 +0100

    layout fix

--- app/(app)/layout.jsx
+++ app/(app)/layout.jsx
@@ -5,7 +5,7 @@ import BottomNav from "@/components/BottomNav";
 
 export default function AppLayout({ children }) {
     return (
-        <div className="flex flex-col  overflow-hidden">
+        <div>
             <Navbar />
             <div className="flex flex-1 relative w-full overflow-hidden">
                 <Sidebar />
